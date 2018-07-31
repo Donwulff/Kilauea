7f6d27412ec026f3e2f544e4bffd0dfd3cfe06ec
EC2 Default User <ec2-user@ip-172-26-15-245.us-east-2.compute.internal>
2018-07-31 20:29:55 +0000

    Handle symlinked directories; only move files

--- all_index.sh
+++ all_index.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-for i in *cam; do echo ">>>>>>>>> $i"; cd $i; ls *2018* | cut -d'_' -f1-3 | uniq | xargs -IX ../index.pl X_00_00.jpg 2; cd ..; done
+DIR=`pwd`
+for i in *cam; do echo ">>>>>>>>> $i"; cd $i; ls *2018*.jpg | cut -d'_' -f1-3 | uniq | xargs -IX $DIR/move.pl X_00_00.jpg 1; cd ..; done
 
